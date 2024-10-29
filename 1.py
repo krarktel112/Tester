21302af799443c4f2cb5daddb32f22fed65d80f0
krarktel112 <krarktel@gmail.com>
2024-10-29 15:32:06 -0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,7 +8,7 @@ tempfile.tempdir = '/home/user/my_tmp'
 #logging.basicConfig()
 #logging.getLogger().setLevel(logging.DEBUG)
 
-firefox_bin = "/snap/firefox/current/usr/lib/firefox/firefox"
+firefox_bin = "/snap/firefox/current/usr/lib/firefox"
 firefoxdriver_bin = "/snap/firefox/current/usr/lib/firefox/geckodriver"
 
 options = webdriver.FirefoxOptions()
