7221cbb6041842e42cc7b178c92bd5b7cdb4d6f6
krarktel112 <krarktel@gmail.com>
2024-10-30 21:33:11 -0400

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+options = webdriver.ChromeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.google.com")
+driver.save_screenshot("/sdcard/download/screenshot.png")
+print("Please check screenshot image")
+driver.quit()
