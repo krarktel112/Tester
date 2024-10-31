c12e07f7d59ce8fd50e55cfe1674e13715f5676b
krarktel112 <krarktel@gmail.com>
2024-10-30 22:28:42 -0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -13,15 +13,6 @@ import logging
 import selenium.webdriver
 import selenium.webdriver.firefox.service
 from selenium import webdriver
-options = webdriver.ChromeOptions()
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--headless=new")
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.google.com")
-driver.save_screenshot("/sdcard/download/screenshot.png")
-print("Please check screenshot image")
-driver.quit()
 
 
 MOZILLA_UAS = 'Mozilla/5.0 (X11; U; Linux i686; en-US) ' \
