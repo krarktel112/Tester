45dc722870aa88f1137a6fac707febd6408e3a08
krarktel112 <krarktel@gmail.com>
2024-10-31 16:45:52 -0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -50,7 +50,7 @@ def fb_hack(email, codex, respect):
   #element = wait.until(EC.visibility_of_element_located((by = By.ID, "element_id")))
   search_box = driver.find_element(by = By.ID, value = "identify_email")
   search_box.send_keys(email)
-  ac = ActionChains(browser)
+  ac = ActionChains(driver)
   ac.move_to_element(elem).move_by_offset(89, 36).click().perform()
   sleep(2)
   driver.save_screenshot("/sdcard/download/tester.png")
