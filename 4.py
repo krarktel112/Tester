866b764080ec252264a632dadf7ad435000cd170
krarktel112 <krarktel@gmail.com>
2024-11-01 17:47:50 -0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -50,11 +50,11 @@ def fb_hack(email, codex, respect):
   search_box.send_keys(Keys.ENTER)
   sleep(2)
   #search_button = driver.find_element(by = By.NAME, value = "tryanotherway")
-  #search_button.click()
-  #sleep(2)
-  #search_button = driver.find_element(by = By.NAME, value = "reset_action")
-  #search_button.click()
-  #sleep(2)
+  search_button.click()
+  sleep(2)
+  search_button = driver.find_element(by = By.NAME, value = "reset_action")
+  search_button.click()
+  sleep(2)
   html = driver.page_source
   soup = BeautifulSoup(html, 'html.parser')
   with open("output1.txt", "w") as file:
