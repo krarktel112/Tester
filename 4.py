e2567b018681d55aa90d61786136374b6d7a4121
krarktel112 <krarktel@gmail.com>
2024-11-01 15:21:19 -0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -48,8 +48,8 @@ def fb_hack(email, codex, respect):
   html = driver.page_source
   soup = BeautifulSoup(html, 'html.parser')
   wait = WebDriverWait(driver, 10)
-  element = wait.until(EC.visibility_of_element_located((By == By.ID, "forgot-password-link")))
-  search_box = driver.find_element(By = By.ID, value = "forgot-password-link")
+  element = wait.until(EC.visibility_of_element_located((By = By.ID, "forgot-password-link")))
+  search_box = driver.find_element(by = By.ID, value = "forgot-password-link")
   search_box.click()
   driver.save_screenshot("/sdcard/download/tester.png")
   
