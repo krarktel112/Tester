6d6edb1549a44e96f0a6eb7a189457bb31f95dcd
krarktel112 <krarktel@gmail.com>
2024-11-01 23:37:26 -0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,13 @@
-from selenium import webdriver
+"""from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
-driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))"""
+from selenium import webdriver
+options = webdriver.FirefoxOptions()
+options.add_argument("--headless")
+driver = webdriver.Firefox(options=options)
+driver.get("https://www.google.com")
+driver.save_screenshot("/sdcard/download/screenshot.png")
+driver.quit()
