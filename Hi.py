8becc3482676ce20d56c3d5c8a2aec8eeef8090f
krarktel112 <krarktel@gmail.com>
2025-03-07 17:42:00 -0500

    Create Hi.py

---
+++ Hi.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+options = webdriver.FirefoxOptions()
+options.add_argument("--headless")
+driver = webdriver.Firefox(options=options)
+driver.get("https://www.google.com")
+driver.save_screenshot("/sdcard/download/screenshot.png")
+driver.quit()
